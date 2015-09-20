35942a8b9866674407860d9e78e8655a5d069492
Rafaextremist <extremist_alive@hotmail.com>
2015-09-20 13:36:05 -0700

    Me falta mucho y tengo muchas dudas

--- Perceptron.py
+++ Perceptron.py
@@ -1,8 +1,9 @@
 
 # Algoritmo PLA perceptro
 # Rafael Noriega
-import random
 import numpy as np
+import random
+
 
 def modelo_aleatorio():
 	#numeros aleatorios entre cero y uno
@@ -20,4 +21,25 @@ def modelo_aleatorio():
 	print y1
 	print y2
 
-modelo_aleatorio()
+	#creo qu es de otra forma? y no asi algo no me cuadra del todo
+	k_2 = 1
+	k_1 = (y2-y1)/(x2-x1)
+	k_0 = y1-k1*x1
+
+#modelo_aleatorio()
+
+def genera_datos(n):
+	#matriz x de n renglones y 2 columnas valores entre 0 y 1
+	x = np.random.rand(n,2)
+
+matriz=genera_datos(2)
+print matriz
+
+def discriminante_lineal(k_0,k_1,k_2):# falta recibir la matriz
+	
+	np.sign(k_0 + k_1 * x1 + k_2 * x2)
+
+def PLA():
+	#me falta mucho :(
+
+def error_clasificacion():
