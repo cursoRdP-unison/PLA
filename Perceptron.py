e4ea2b561176bb9c30694be81abf06658ef4769e
cruzluquejm <cruzluquejm@gmail.com>
2015-09-22 07:44:32 -0700

    Ahi va creo ..

--- Perceptron.py
+++ Perceptron.py
@@ -18,7 +18,7 @@ def modelo_aleatorio():
 
 def genera_datos(N):
 
-	return None if N <= 0 else np.random.uniform(0, 1, size=(N, 2))
+	return None if N <= 0 else np.column_stack((np.ones(N), (np.random.uniform(0, 1, size=(N, 2)))))
 
 def discriminante_lineal(k_0, k_1, k_2, X):
 
@@ -26,10 +26,9 @@ def discriminante_lineal(k_0, k_1, k_2, X):
 	temp = 0
 
 	for i in xrange(X.shape[0]):
-		x_1 = X[temp][0]
-		x_2 = X[temp][1]
+		x_1 = X[i][0]
+		x_2 = X[i][1]
 		Y.append(np.sign(k_0 + k_1 * x_1 + k_2 * x_2))
-		temp += 1
 
 	return Y
 
@@ -37,8 +36,12 @@ def PLA(X, Y):
 
 	w = np.random.uniform(0, 1, size=3)
 
+	rand = 
 	Xi = X[np.random.choice(X.shape[0])]
 
+	z = np.dot(w.transpose(), Xi)
+
+	print z
 		
 k_0, k_1, k_2 = modelo_aleatorio()
 
