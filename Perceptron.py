ea563627adb8ad4d4b5555f88f7f87b0ef62cd16
cruzluquejm <cruzluquejm@gmail.com>
2015-09-22 04:29:41 -0700

    Un par de dudas :/

--- Perceptron.py
+++ Perceptron.py
@@ -10,15 +10,40 @@ def modelo_aleatorio():
 	x2 = np.random.uniform(0,1)
 	y2 = np.random.uniform(0,1)
 
-	k_2 = 1 
+	k_2 = 1.0 
 	k_1 = (y2 - y1) / (x2 - x1) 
 	k_0 = y1 - k_1 * x1 
 
 	return k_0, k_1, k_2
 
 def genera_datos(N):
-	return None if N <= 0 else np.random.uniform(0, 1, size=(N,2))
-	
+
+	return None if N <= 0 else np.random.uniform(0, 1, size=(N, 2))
+
+def discriminante_lineal(k_0, k_1, k_2, X):
+
+	Y = []
+	temp = 0
+
+	for i in xrange(X.shape[0]):
+		x_1 = X[temp][0]
+		x_2 = X[temp][1]
+		Y.append(np.sign(k_0 + k_1 * x_1 + k_2 * x_2))
+		temp += 1
+
+	return Y
+
+def PLA(X, Y):
+
+	w = np.random.uniform(0, 1, size=3)
+
+	Xi = X[np.random.choice(X.shape[0])]
+
+		
 k_0, k_1, k_2 = modelo_aleatorio()
 
 X = genera_datos(5)
+
+Y = discriminante_lineal(k_0, k_1, k_2, X)
+
+PLA(X, Y)
