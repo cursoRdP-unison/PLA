ae0d4c6d026aa279d1e977e0c47ac9b4d9c4ba72
Rafaextremist <extremist_alive@hotmail.com>
2015-12-14 20:41:26 -0700

    ya mero falta poco
    continuacion desde donde me quede

--- Perceptron.py
+++ Perceptron.py
@@ -26,20 +26,65 @@ def modelo_aleatorio():
 	k_1 = (y2-y1)/(x2-x1)
 	k_0 = y1-k1*x1
 
-#modelo_aleatorio()
+	return k_0,k_1,k_2
+
 
 def genera_datos(n):
 	#matriz x de n renglones y 2 columnas valores entre 0 y 1
 	x = np.random.rand(n,2)
-
-matriz=genera_datos(2)
-print matriz
-
-def discriminante_lineal(k_0,k_1,k_2):# falta recibir la matriz
+	return x
 	
-	np.sign(k_0 + k_1 * x1 + k_2 * x2)
+def discriminante_lineal(k_0,k_1,k_2,x):
+	
+	Y = []
+    for i in xrange(X.shape[0]):
+        x_1 = X[i][1]
+        x_2 = X[i][2]
+        y.append(np.sign(k_0 + k_1 * x_1 + k_2 * x_2))
+	return y
 
-def PLA():
-	#me falta mucho :(
+def PLA(X,Y):
+	max_iter = 0
+    w = np.random.uniform(0, 1, size=X.shape[1])
+    Ye = np.sign(np.dot(X, w)) 
+    error = Ye * Y
+    
+    while any(e != 1 for e in error) and max_iter < 1000:
+        i = np.random.choice(X.shape[0])
+        sign = np.sign(np.dot(X[i], w))
 
-def error_clasificacion():
+        if sign == Y[i]:
+            pass
+        else:
+            w = w + np.dot(X[i], Y[i])
+            Ye = np.sign(np.dot(X, w)) 
+            error = Ye * Y
+        max_iter += 1
+    return w
+	
+def error_clasificacion(Y, Y_e):
+
+    c = 0
+    for i in xrange(len(Y)):
+        if Y[i] != Y_e[i]:
+            c += 1
+    return c / float(len(Y))
+	
+def E_o_prom(n):
+
+    E_lista = np.zeros(500)
+    X_o = genera_datos(10000)
+
+    for epoch in xrange(1, 100):
+        k_0, k_1, k_2 = modelo_aleatorio()
+        Y_o = discriminante_lineal(k_0, k_1, k_2, X_o)
+
+        for iter in xrange(1, 100):
+            X = genera_datos(N)
+            Y = discriminante_lineal(k_0, k_1, k_2, X)
+            w_0, w_1, w_2 = PLA(X, Y)
+            Y_eo = discriminante_lineal(w_0, w_1, w_2, X_o)
+            E_o = error_clasificacion(Y_o, Y_eo)
+            indice = 100 * epoch + iter
+
+    return sum(E_lista) / len(E_lista)
